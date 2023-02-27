30a59b645842f894d62570d8350d7086cd2f4751
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-02-27 19:43:22 +0300

    Blog single pages added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
-const Blog = require('./models/Blog')
+const Blog = require('./models/Blog');
 
 const app = express();
 
@@ -14,26 +14,34 @@ app.set('view engine', 'ejs');
 
 //! Middlewares
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //! Routes
-app.get('/', async (req,res) => {
+app.get('/', async (req, res) => {
   const blogs = await Blog.find({});
   res.render('index', {
-    blogs
+    blogs,
   });
 });
 
-app.get('/about', (req,res) => {
+app.get('/blogs/:id', async (req, res) => {
+  //console.log(req.params.id);
+  const blog = await Blog.findById(req.params.id);
+  res.render('blog', {
+    blog,
+  });
+});
+
+app.get('/about', (req, res) => {
   res.render('about');
 });
 
-app.get('/add_post', (req,res) => {
+app.get('/add_post', (req, res) => {
   res.render('add_post');
 });
 
-app.post('/blogs', async (req,res) => {
+app.post('/blogs', async (req, res) => {
   await Blog.create(req.body);
   res.redirect('/');
 });
