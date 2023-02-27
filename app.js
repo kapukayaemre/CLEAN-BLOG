717d2a17665409417f888e7c558889ea26dffc8f
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-02-28 01:23:02 +0300

    MVC integrated

--- app.js
+++ app.js
@@ -1,52 +1,59 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const ejs = require('ejs');
-const path = require('path');
-const Blog = require('./models/Blog');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const ejs = require("ejs");
+const methodOverride = require("method-override");
 
 const app = express();
 
-//! Connect DB
-mongoose.connect('mongodb://127.0.0.1:27017/blog-db');
+const Post = require("./models/Post");
+const {
+  getAllPosts,
+  getPost,
+  createPost,
+  updatePost,
+  deletePost
+} = require("./controllers/postController");
+const {
+  aboutPage,
+  homePage,
+  addPage,
+  editPage
+} = require("./controllers/pageController");
 
-//! Template Engine
-app.set('view engine', 'ejs');
+// TEMPLATE ENGINE
+app.set("view engine", "ejs");
 
-//! Middlewares
-app.use(express.static('public'));
+// MIDDLEWARE
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride("_method", { methods: ["POST", "GET"] }));
 
-//! Routes
-app.get('/', async (req, res) => {
-  const blogs = await Blog.find({});
-  res.render('index', {
-    blogs,
-  });
-});
+// ROUTES
+app.get("/", getAllPosts); // home page
+app.get("/post/:id", getPost); // post detail page
+app.post("/add", createPost); // add new post
+app.put("/post/:id", updatePost); // update post
+app.delete("/post/:id", deletePost); // delete post
+app.get("/home", homePage); // home page
+app.get("/about", aboutPage); // about page
+app.get("/add", addPage); // add post page
+app.get("/post/edit/:id", editPage); // edit post page
 
-app.get('/blogs/:id', async (req, res) => {
-  //console.log(req.params.id);
-  const blog = await Blog.findById(req.params.id);
-  res.render('blog', {
-    blog,
-  });
-});
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
-app.get('/add_post', (req, res) => {
-  res.render('add_post');
-});
-
-app.post('/blogs', async (req, res) => {
-  await Blog.create(req.body);
-  res.redirect('/');
-});
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Sunucu ${port} portunda başlatıldı...`);
-});
+mongoose.set("strictQuery", false);
+mongoose.connect(
+  "mongodb://127.0.0.1:27017/blog-db",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  err => {
+    if (err) throw err;
+    console.log("Connected to MongoDB");
+    const PORT = 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  }
+);
