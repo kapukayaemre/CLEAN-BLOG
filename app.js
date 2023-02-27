5cd80c913f75ebb40d1b1e498ca152a3275fd228
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-02-27 04:41:57 +0300

    View pages created and partials created,integreted

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
-
 
 const app = express();
 
-app.get('/', (req,res) => {
-  
-})
+//! Template Engine
+app.set('view engine', 'ejs');
 
 //! Middlewares
 app.use(express.static('public'));
+
+//! Routes
+app.get('/', (req,res) => {
+  res.render('index');
+});
+
+app.get('/about', (req,res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req,res) => {
+  res.render('add_post');
+});
 
 const port = 3000;
 app.listen(port, () => {
